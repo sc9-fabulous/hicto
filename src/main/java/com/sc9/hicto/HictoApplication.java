4d0bc30e58c2a41a4219c09075382c78a01b76ad
Cornelii <cornelii.g.son@gmail.com>
2022-07-31 06:43:10 +0900

    first commit

---
+++ src/main/java/com/sc9/hicto/HictoApplication.java
@@ -0,0 +1,13 @@
+package com.sc9.hicto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HictoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HictoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sc9/hicto/HictoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc9.hicto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HictoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
